d96a249ce42cfe79bb3ab61b8c84ccebcfbb284b
AmosRM <88614465+AmosRM@users.noreply.github.com>
2022-05-18 12:09:41 +0800

    .py add

--- python-Amos/altaircreate.py
+++ python-Amos/altaircreate.py
@@ -44,10 +44,12 @@ for i in range(len(route_df)):
         
 route_df['distance'] = distances
 
+skip = 10
+route_df = route_df[::skip]
 
 route_df['elevation_diff'] = route_df['elevation'].diff()
 route_df['time_diff'] = [0] + list((route_df.loc[1:,'time'].values - route_df.loc[:(route_df.shape[0]-2),'time'].values)/np.timedelta64(1,'s'))
-route_df['speed'] = route_df['distance']/route_df['time_diff']
+route_df['speed'] = route_df['distance']/route_df['time_diff'] * 18 / 5 * 10
 route_df['total_dist'] = np.cumsum(route_df['distance'])
 route_df['km'] = np.ceil((route_df['total_dist']+0.01)/1000)
 route_df = route_df.fillna(0)
@@ -61,3 +63,45 @@ grouped.columns = grouped.columns.get_level_values(0)
 
 route_df = route_df.join(grouped.loc[:, ['net_elevation','net_time']], how='left', on='km')
 
+df = route_df[['latitude', 'longitude', 'elevation', 'time', 'distance',
+       'elevation_diff', 'time_diff', 'speed', 'total_dist', 'km',
+       'net_elevation']]
+df['total_dist'] = df['total_dist']/100
+df['speed-km/h'] = df['speed'].rolling(30).mean()
+
+# ALTAIR VIZ
+
+line = alt.Chart(df.iloc[::50, :]).mark_line().encode(
+    x = alt.X('total_dist'),
+    y = alt.Y('elevation')
+)
+nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['total_dist'], empty='none')
+
+selectors = alt.Chart(df.iloc[::50, :]).mark_point().encode(
+    x='total_dist',
+    opacity=alt.value(0),
+).add_selection(
+    nearest
+)
+points = line.mark_point(color='red').encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+)
+rules = alt.Chart(df.iloc[::50, :]).mark_rule(color='gray').encode(
+    x='total_dist',
+).transform_filter(
+    nearest
+)
+speed = alt.Chart(df.iloc[::50, :]).mark_line().encode(
+    x = alt.X('total_dist'),
+    y = alt.Y('speed-km/h'),
+    order='total_dist'
+)
+a = line.mark_area(opacity=0.5) + selectors + points + rules
+b = speed + rules
+
+final = alt.layer(a,b).resolve_scale(
+    y = 'independent'
+)
+
+final
